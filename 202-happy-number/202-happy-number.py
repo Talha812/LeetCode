62ae50a8809828312383f3deb691bd6109ad8893
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-05-15 20:52:32 +0500

    Time: 54 ms (34.73%), Space: 13.9 MB (14.96%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        ans = n
+        stack = []
+        while(ans > 1):
+            summ = 0
+            while(n>0):
+                summ += pow(n%10, 2)
+                n = n//10
+            
+            ans = summ
+            n = summ
+            if(summ in stack):
+                return False
+            else:
+                stack.append(summ)
+                
+            if(ans == 1):
+                return True
+        
+        return True
+        
