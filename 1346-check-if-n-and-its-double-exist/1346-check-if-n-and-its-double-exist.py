e8605df9ce938661f43fa7ecc4cefaa504fb240e
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-05-18 16:57:56 +0500

    Time: 96 ms (25.78%), Space: 13.9 MB (91.10%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -10,16 +10,4 @@ class Solution:
                 dic[i] = i/2
         
         return False
-        
-#         if(arr.count(0) == 2):
-#             return True
-        
-#         for i in range(len(arr)-1):
-#             for j in range(i+1, len(arr)):
-#                 if(arr[i] != 0 and arr[j] != 0):
-#                     if(arr[i]/arr[j] == 2 or arr[j]/arr[i] == 2):
-#                         return True
-
-        
-#         return False
                     
