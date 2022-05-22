776a8be7022760bf542daca652bfdd932b862738
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-05-22 16:37:59 +0500

    Time: 249 ms (29.73%), Space: 14.5 MB (29.09%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -11,13 +11,3 @@ class Solution:
                     count += 1
             
         return count >= n
-    
-#         count = 0
-#         flowerbed = [0] + flowerbed + [0]  
-        
-#         for i in range(1, len(flowerbed)-1): # loop over original flowerbed (1 to len(bed)-1) as now it has 1 extra 0 at start and 1 extra 0 at end.
-#             if flowerbed[i-1] == flowerbed[i] == flowerbed[i+1] == 0:
-#                 count += 1
-#                 flowerbed[i] = 1
-                
-#         return count >= n
