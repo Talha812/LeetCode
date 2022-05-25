6ddaf5f69f0b9a53a05dfebf932ed84c56a3d2ff
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-05-25 21:04:49 +0500

    Time: 36 ms (74.50%), Space: 13.9 MB (67.47%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,24 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        
+        dic = {1:"a", 2:"b", 3:"c", 4:"d", 5:"e", 6:"f", 7:"g", 8:"h", 9:"i", 10:"j", 11:"k", 12:"l", 13:"m", 14:"n", 15:"o", 16:"p", 17:"q", 18:"r", 19:"s", 20:"t", 21:"u", 22:"v", 23:"w", 24:"x", 25:"y", 26:"z"}
+        
+        
+        ans = ""
+        ind = len(s)-1
+        for i in range(len(s)-1, -1, -1):
+            if(s[ind] == "#"):
+                num = s[ind-2] + s[ind-1]
+                ans = dic[int(num)] + ans
+                #print(ans)
+                ind = ind - 3
+            else:
+                ans = dic[int(s[ind])] + ans
+                ind -= 1
+               # print(ans)
+                
+            if(ind < 0 and i >= 0):
+                break
+        
+        return ans
+                
