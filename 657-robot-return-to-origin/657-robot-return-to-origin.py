f67af2eef86b681723c4b4789dc3d24a62440cc7
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-05-27 21:40:39 +0500

    Time: 187 ms (5.08%), Space: 14.2 MB (37.09%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.py
+++ 657-robot-return-to-origin/657-robot-return-to-origin.py
@@ -1,27 +1,52 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
         
+        left = 0
+        right = 0
+        up = 0
+        down = 0
         
-        UD = []
-        LR = []
+        i = 0
+        while(i<len(moves)):
+            if(moves[i] == "U"):
+                up += 1
+            
+            elif(moves[i] == "D"):
+                down += 1
+            
+            elif(moves[i] == "L"):
+                left += 1
+            
+            else:
+                right += 1
+            
+            i += 1
         
-        for i in moves:
-            if i=='U' or i == 'D':
-                if UD == []:
-                    UD.append(i)
-                else:
-                    if UD[-1] == i:
-                        UD.append(i)
-                    else:
-                        UD.pop()
+        if(left == right and up == down):
+            return True
+        
+        return False
+        
+#         UD = []
+#         LR = []
+        
+#         for i in moves:
+#             if i=='U' or i == 'D':
+#                 if UD == []:
+#                     UD.append(i)
+#                 else:
+#                     if UD[-1] == i:
+#                         UD.append(i)
+#                     else:
+#                         UD.pop()
                         
-            elif i=='L' or i == 'R':
-                if LR == []:
-                    LR.append(i)
-                else:
-                    if LR[-1] == i:
-                        LR.append(i)
-                    else:
-                        LR.pop()
+#             elif i=='L' or i == 'R':
+#                 if LR == []:
+#                     LR.append(i)
+#                 else:
+#                     if LR[-1] == i:
+#                         LR.append(i)
+#                     else:
+#                         LR.pop()
                         
-        return UD == LR
+#         return UD == LR
