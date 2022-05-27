caa7fd2e23aeb0c5322138d6f24c37c255814e85
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-05-27 21:34:00 +0500

    Time: 204 ms (5.08%), Space: 14.2 MB (37.09%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.py
@@ -0,0 +1,27 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        
+        
+        UD = []
+        LR = []
+        
+        for i in moves:
+            if i=='U' or i == 'D':
+                if UD == []:
+                    UD.append(i)
+                else:
+                    if UD[-1] == i:
+                        UD.append(i)
+                    else:
+                        UD.pop()
+                        
+            elif i=='L' or i == 'R':
+                if LR == []:
+                    LR.append(i)
+                else:
+                    if LR[-1] == i:
+                        LR.append(i)
+                    else:
+                        LR.pop()
+                        
+        return UD == LR
