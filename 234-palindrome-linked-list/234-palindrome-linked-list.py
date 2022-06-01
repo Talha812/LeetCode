faea1eac539422a55858acfb549fb35b6e3580c0
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-06-01 20:32:22 +0500

    Time: 1026 ms (49.96%), Space: 39.2 MB (69.22%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        if not head or not head.next : 
+            return True
+        
+        def getMidPoint(curr):
+            slow = curr
+            fast = curr
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+        
+        def reverseLinkedList(currN):
+            prev = None
+            curr = currN
+            while curr:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            return prev
+        
+        mid = getMidPoint(head)
+        revHead = reverseLinkedList(mid)
+        
+        while revHead:# and head and revHead != head:
+            if revHead.val != head.val:
+                return False
+            head = head.next
+            revHead = revHead.next
+        
+        return True
