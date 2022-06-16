25e8d248a99346e380b8dcf6c1ae38fba1369d20
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-06-16 19:39:59 +0500

    Time: 1845 ms (5.07%), Space: 14.2 MB (83.66%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -3,14 +3,14 @@ class Solution:
         
         ans = []
         
-        distinct_num1 = []
+        distinct_num1 = []      #stores values which are not in nums2
         for val in nums1:
             if val not in nums2 and val not in distinct_num1:
                 distinct_num1.append(val)
         
         ans.append(distinct_num1)
         
-        distinct_num2 = []
+        distinct_num2 = []      #stores values which are not in nums1
         for val in nums2:
             if val not in nums1 and val not in distinct_num2:
                 distinct_num2.append(val)
