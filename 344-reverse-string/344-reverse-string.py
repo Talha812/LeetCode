1c55dd67a4462068e8214aa5ab6269ac25cae496
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-06-20 18:44:26 +0500

    Time: 358 ms (20.14%), Space: 18.4 MB (87.41%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,9 +4,13 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
-        i=0
-        j=len(s)-1
-        while(i<j):
-            s[i], s[j] = s[j], s[i]
+        i = 0
+        j = len(s)-1
+        while(i < j):
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
             i += 1
             j -= 1
+            
+        
