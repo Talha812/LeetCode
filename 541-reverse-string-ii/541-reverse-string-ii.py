4a7544fac47eb4dac2900a06e73079b052717871
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-06-22 16:29:15 +0500

    Time: 63 ms (22.82%), Space: 14 MB (56.24%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        
+        def reverse_String(arr, start, end):
+            i = start
+            j = end
+            while(i < j):
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+                i += 1
+                j -= 1
+            #print(arr)
+            return arr
+        
+        arr = []
+        for ch in s:
+            arr.append(ch)
+        
+        start = 0
+        
+        while(start < len(arr)):
+            end = min(start+k-1,len(arr)-1)
+            reverse_String(arr,start, end)
+            start += 2*k
+        
+        return "".join(arr)
+            
