c96077847306c9c954b3a89405446fa31891685b
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-09-26 19:00:51 +0500

    Time: 129 ms (5.18%), Space: 14 MB (47.76%) - LeetHub

--- 1417-reformat-the-string/1417-reformat-the-string.py
+++ 1417-reformat-the-string/1417-reformat-the-string.py
@@ -1,52 +1,40 @@
 class Solution:
     def reformat(self, s: str) -> str:
         
-        digits = 0
-        str_digits = ""
+        characters = ""
+        digits = ""
+        
         for i in s:
-            if i == "0" or i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9":
-                digits += 1
-                str_digits += i
-       #print(str_digits)
-    
-        alphabets = 0
-        str_alphabets = ""
-        for i in s:
-            if(ord(i) >= 97 and ord(i) <= 122):
-                alphabets += 1
-                str_alphabets += i
-        
-        #print(str_alphabets)
-        
-        possible = False
-        
-        if(digits-alphabets == 0 or digits-alphabets == -1 or digits-alphabets == 1):
-            possible = True
-        
-        if possible:
-            ans = ""
-            if(digits-alphabets == 0 or digits-alphabets == 1):
-                sInd = 0
-                dInd = 0
-                for i in range(len(s)):
-                    if i%2 == 0:
-                        ans += str_digits[dInd]
-                        dInd += 1
-                    else:
-                        ans += str_alphabets[sInd]
-                        sInd += 1
-            
+            if i.isdigit():
+                digits += i
             else:
-                sInd = 0
-                dInd = 0
-                for i in range(len(s)):
-                    if i%2 == 0:
-                        ans += str_alphabets[sInd]
-                        sInd += 1
-                    else:
-                        ans += str_digits[dInd]
-                        dInd += 1
-                
-            return ans
+                characters += i
+        
+        digits_count = len(digits)
+        char_count = len(characters)
+        
+        if((char_count-digits_count > 1) or (char_count-digits_count < -1)):
+            return ""
+        
+        ans = ""
+        sInd = 0
+        dInd = 0
+        if(digits_count-char_count == 0 or digits_count-char_count == 1):
+            for i in range(len(s)):
+                if i%2 == 0:
+                    ans += digits[dInd]
+                    dInd += 1
+                else:
+                    ans += characters[sInd]
+                    sInd += 1
             
-        return ""
+        else:
+            for i in range(len(s)):
+                if i%2 == 0:
+                    ans += characters[sInd]
+                    sInd += 1
+                else:
+                    ans += digits[dInd]
+                    dInd += 1
+                
+        return ans
