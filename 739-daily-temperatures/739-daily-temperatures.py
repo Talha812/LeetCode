963a8f2464f0859a0d2d3471e091deef71249916
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-10-08 22:02:23 +0500

    Time: 1268 ms (80.37%), Space: 25 MB (52.72%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -12,21 +12,3 @@ class Solution:
             stack.append([t, i])
             
         return res
-#         ans = []
-        
-#         for i in range(len(temperatures)):
-#             count = 0
-#             found = False
-#             for j in range(i+1, len(temperatures)):
-#                 if(temperatures[j] > temperatures[i]):
-#                     found = True
-#                     count += 1
-#                     break
-#                 if(found == False):
-#                     count += 1
-#             if(found):        
-#                 ans.append(count)
-#             else:
-#                 ans.append(0)
-        
-#         return ans
