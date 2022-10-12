3044bf4d7d9d85cc773e5200e7bef8029af7a64f
Talha812 <72699916+Talha812@users.noreply.github.com>
2022-10-12 19:44:24 +0500

    Time: 242 ms (18.66%), Space: 13.8 MB (82.34%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,79 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        #checking Rows Fully
+        for i in range(len(board)):
+            hold_dic = {}
+            for j in range(len(board[i])):
+                if board[i][j] in hold_dic and board[i][j] != ".":
+                    return False
+                else:
+                    hold_dic[board[i][j]] = 1
+
+                    
+        #checking Columns Fully
+        for i in range(len(board)):
+            hold_dic = {}
+            for j in range(len(board[i])):
+                if board[j][i] in hold_dic and board[j][i] != ".":
+                    return False
+                else:
+                    hold_dic[board[j][i]] = 1
+                    
+        
+        def checking3x3matrix(array,start,end,colStart,colEnd):
+            dic = {}
+            for i in range(start,end):
+                for j in range(colStart,colEnd):
+                    if(array[i][j] in dic and array[i][j] != "."):
+                        return False
+                    else:
+                        dic[array[i][j]] = 1   
+            return True            
+        
+        matrix1 = checking3x3matrix(board,0,3,0,3)
+        #print("matrix1", matrix1)
+        if(not matrix1):
+            return False
+        
+        matrix2 = checking3x3matrix(board,0,3,3,6)
+        #print("matrix2",matrix2)
+        if(not matrix2):
+            return False
+        
+        matrix3 = checking3x3matrix(board,0,3,6,9)
+        #print("matrix3",matrix3)
+        if(not matrix3):
+            return False
+        
+        matrix4 = checking3x3matrix(board,3,6,0,3)
+        #print("matrix4",matrix4)
+        if(not matrix4):
+            return False
+        
+        matrix5 = checking3x3matrix(board,3,6,3,6)
+        #print("matrix5",matrix5)
+        if(not matrix5):
+            return False
+        
+        matrix6 = checking3x3matrix(board,3,6,6,9)
+        #print("matrix6",matrix6)
+        if(not matrix6):
+            return False
+        
+        matrix7 = checking3x3matrix(board,6,9,0,3)
+        #print("matrix7",matrix7)
+        if(not matrix7):
+            return False
+        
+        matrix8 = checking3x3matrix(board,6,9,3,6)
+        #print("matrix8",matrix8)
+        if(not matrix8):
+            return False
+        matrix9 = checking3x3matrix(board,6,9,6,9)
+        #print("matrix9",matrix9)
+        if(not matrix9):
+            return False
+        
+        return True
+        
