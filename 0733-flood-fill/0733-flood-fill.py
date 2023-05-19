0a841bde924f60a876a0c026ba55180cf6fbcd30
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-05-19 19:09:18 +0500

    Time: 97 ms (6.53%), Space: 16.4 MB (20.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,19 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        def fill(image, sr, sc, newColor, str_pixel):
-            if sr < 0 or sr > len(image)-1 or sc < 0 or sc > len(image[0])-1 or image[sr][sc] == newColor or image[sr][sc] != start_pixel:
+        def fill(image, sr, sc, newColor, start):
+            if sr < 0 or sr > len(image)-1 or sc < 0 or sc > len(image[0])-1 or image[sr][sc] == newColor or image[sr][sc] != start:
                 return
             
             image[sr][sc] = newColor
-            fill(image, sr + 1, sc, newColor, start_pixel)
-            fill(image, sr - 1, sc, newColor, start_pixel)
-            fill(image, sr, sc + 1, newColor, start_pixel)
-            fill(image, sr, sc - 1, newColor, start_pixel)
+            fill(image, sr + 1, sc, newColor, start)
+            fill(image, sr - 1, sc, newColor, start)
+            fill(image, sr, sc + 1, newColor, start)
+            fill(image, sr, sc - 1, newColor, start)
             
         
         newColor = color
-        start_pixel = image[sr][sc]
-        fill(image, sr, sc, newColor, start_pixel)
+        start = image[sr][sc]
+        fill(image, sr, sc, newColor, start)
         
         return image
