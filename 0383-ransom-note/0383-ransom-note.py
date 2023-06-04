ff886a4ba1314dc2361ceb0e8485913303bb4317
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-06-04 21:51:50 +0500

    Time: 86 ms (31.03%), Space: 16.6 MB (39.41%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,24 +1,22 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
+        if len(ransomNote) > len(magazine):
+            return False
+        
         dic1 = {}
-        dic2 = {}
-        
-        for i in ransomNote:
-            if i not in dic1:
-                dic1[i] = 1
+                
+        for ch in magazine:
+            if ch in dic1:
+                dic1[ch] += 1
             else:
-                dic1[i] += 1
-        
-        for i in magazine:
-            if i not in dic2:
-                dic2[i] = 1
+                dic1[ch] = 1
+            
+        for ch in ransomNote:
+            if ch in dic1 and dic1[ch] > 0:
+                dic1[ch] -= 1
+            
             else:
-                dic2[i] += 1
-        
-        
-        for k in dic1:
-            if k not in dic2 or dic1[k] > dic2[k]:
                 return False
         
         return True
