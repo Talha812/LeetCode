2401be34d45770fd2babd080db9c722a7401a1c3
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-06-13 17:40:41 +0500

    Time: 225 ms (15.08%), Space: 16.9 MB (30.34%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -15,13 +15,7 @@ class Solution:
             ball, r, c = q.popleft()
             if r == len(grid):
                 ans[ball] = c            
-                
-            # elif c < 0 or c >= len(grid[0]):
-            #     ans[ball] = -1
-            
-            # if(c+1 < len(grid[0]) and grid[r][c] == 1 and grid[r][c+1] == -1) or (c-1 >= 0 and grid[r][c] == -1 and grid[r][c-1] == 1):
-            #     ans[ball] = -1 
-                
+                                
             elif c+1 < len(grid[0]) and grid[r][c] == 1 and grid[r][c+1] == 1:
                 q.append((ball, r+1,c+1))
                     
