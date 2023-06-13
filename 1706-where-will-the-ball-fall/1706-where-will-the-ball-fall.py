bc1952a59682ed12c8771dea401702bd1938c96c
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-06-13 17:40:23 +0500

    Time: 235 ms (8.05%), Space: 16.8 MB (52.03%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        
+        ans = [-1]*len(grid[0])
+        
+        col = len(grid[0])
+        row = len(grid)
+        
+        q = collections.deque([])
+        for j in range(col):
+            ball = j
+            q.append((ball,0,j))
+
+        while q:
+            ball, r, c = q.popleft()
+            if r == len(grid):
+                ans[ball] = c            
+                
+            # elif c < 0 or c >= len(grid[0]):
+            #     ans[ball] = -1
+            
+            # if(c+1 < len(grid[0]) and grid[r][c] == 1 and grid[r][c+1] == -1) or (c-1 >= 0 and grid[r][c] == -1 and grid[r][c-1] == 1):
+            #     ans[ball] = -1 
+                
+            elif c+1 < len(grid[0]) and grid[r][c] == 1 and grid[r][c+1] == 1:
+                q.append((ball, r+1,c+1))
+                    
+            elif c-1 >= 0 and grid[r][c] == -1 and grid[r][c-1] == -1:
+                q.append((ball, r+1, c-1))
+        
+            
+                
+        return ans
+            
+            
+                    
+                    
+            
+            
