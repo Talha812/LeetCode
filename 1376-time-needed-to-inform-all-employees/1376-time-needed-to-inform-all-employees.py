66d69fa05f91cd269c3e94603f906395a3fa3018
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-07-11 16:21:42 +0500

    Time: 1448 ms (15.63%), Space: 44 MB (77.84%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numOfMinutes(self, n, headID, manager, informTime):
+        
+        if n == 1:
+            return informTime[0]
+        
+        head_emp = {}
+        
+        for i in range(len(manager)):                
+            if manager[i] not in head_emp:
+                head_emp[manager[i]] = [(i, informTime[manager[i]])]
+                
+            else:
+                head_emp[manager[i]].append((i, informTime[manager[i]]))
+        
+        #print(head_emp)
+        
+        queue = head_emp[headID]
+        #print(queue)
+        
+        max_time = 0
+        while queue:
+            mana_ger, time = queue.pop(0)
+            #print(mana_ger, time)
+            
+            if mana_ger in head_emp:
+                for e, t in head_emp[mana_ger]:
+                    queue.append((e, t+time))
+                    max_time = max(max_time, t+time)
+                    # else:
+                    #     max_time = max(max_time, time)
+            
+            else:
+                max_time = max(max_time, time)
+            
+        return max_time
+
+            
