8bd8d12617f67fac335fcc91e1cd98c7d7f9c84d
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-07-19 19:02:21 +0500

    Time: 758 ms (38.85%), Space: 22.1 MB (31.54%) - LeetHub

--- 0911-online-election/0911-online-election.py
+++ 0911-online-election/0911-online-election.py
@@ -2,46 +2,43 @@ class TopVotedCandidate(object):
 
     def __init__(self, persons: List[int], times: List[int]):
         
-        count = {}
-        mostVotePersons = [0] * len(persons)
-        largestVoteInd = -1
+        vote_count = {}
+        winner = [0] * len(persons)
+        mostVotePerson = -1
+        
         for i in range(len(persons)):
-            if persons[i] not in count:
-                count[persons[i]] = 1
+            if persons[i] in vote_count:
+                vote_count[persons[i]] += 1
             else:
-                count[persons[i]] += 1
-                
-            if largestVoteInd == -1 or count[persons[i]] >= count[largestVoteInd]:
-                largestVoteInd = persons[i]
-                
-            mostVotePersons[i] = largestVoteInd
+                vote_count[persons[i]] = 1
+            
+            if mostVotePerson == -1 or vote_count[persons[i]] >= vote_count[mostVotePerson]:
+                mostVotePerson = persons[i]
+        
+            winner[i] = mostVotePerson
         
         self.times = times
-        self.mostVotePersons = mostVotePersons
+        self.winner = winner
         
-
+        # print(times, winner, vote_count)
+        
     def q(self, t: int) -> int:
-        idx = self.findExactRight(self.times, t) - 1 # binary search on times to find the most recent time before t
-        return self.mostVotePersons[idx]
-    
-    def findExactRight(self, nums, target):
-        """
-        Returns rightmost insertion point that target should be inserted in the sorted array
-        """
-        left, right = 0, len(nums)-1
+        
+        left = 0
+        right = len(self.winner)-1
         
         while left <= right:
-            mid = left + (right - left) // 2
-            
-            if nums[mid] <= target:
+            mid = (right+left)//2
+                        
+            if self.times[mid] <= t:
                 left = mid + 1
+                
             else:
                 right = mid - 1
-            
-        return left
 
+        return self.winner[left-1]
     
-
+        
 # Your TopVotedCandidate object will be instantiated and called as such:
 # obj = TopVotedCandidate(persons, times)
 # param_1 = obj.q(t)
