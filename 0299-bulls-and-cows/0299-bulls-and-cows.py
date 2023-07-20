56c5a8583c9806a057eeeeb661cb89a05d928da0
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-07-20 18:53:34 +0500

    Time: 64 ms (25.87%), Space: 16.3 MB (51.84%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.py
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -1,27 +1,50 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
         
+#         bull = 0
+#         dic1 = {}
+#         dic2 = {}
         
-        map1 = {}
-        map2 = {}
-        bull = 0
-        for i in range(len(secret)):
-            if secret[i] == guess[i]:
-                bull += 1
-            else:
-                if secret[i] in map1:
-                    map1[secret[i]] += 1
-                else:
-                    map1[secret[i]] = 1
+#         for i in range(len(secret)):
+#             if secret[i] == guess[i]:
+#                 bull += 1
+#             else:
+#                 if secret[i] in dic1:
+#                     dic1[secret[i]] += 1
+#                 else:
+#                     dic1[secret[i]] = 1
+                
+#                 if guess[i] in dic2:
+#                     dic2[guess[i]] += 1
+#                 else:
+#                     dic2[guess[i]] = 1
+        
+#         cow = 0
+#         for k in dic1:
+#             if k in dic2:
+#                 cow += min(dic1[k], dic2[k])
+        
+#         return str(bull) + "A" + str(cow) + "B"
 
-                if guess[i] in map2:
-                    map2[guess[i]] += 1
-                else:
-                    map2[guess[i]] = 1
-                           
+
+        bull = 0
+        dic = {}
+        
+        for i in range(len(secret)):
+            if secret[i] in dic:
+                dic[secret[i]] += 1
+            else:
+                dic[secret[i]] = 1
         cow = 0
-        for k in map1:
-            if k in map2:
-                cow += min(map1[k], map2[k])
+        for i in range(len(guess)):
+            if guess[i] == secret[i]:
+                bull += 1
+                dic[secret[i]] -= 1
+                            
+        cow = 0
+        for i in range(len(guess)):
+            if guess[i] in dic and dic[guess[i]] > 0 and guess[i] != secret[i]:
+                cow += 1
+                dic[guess[i]] -= 1
         
         return str(bull) + "A" + str(cow) + "B"
