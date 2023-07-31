8434713f275801e1d10b4a9a4e51b545cbeb9725
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-07-31 18:47:26 +0500

    Time: 521 ms (85.43%), Space: 17.8 MB (37.99%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,32 +1,32 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        def get_neighbors(state):
-            neighbors = []
-            for i in range(4):
-                num = int(state[i])
-                for change in [-1, 1]:
-                    next_num = (num + change) % 10
-                    next_state = state[:i] + str(next_num) + state[i + 1:]
-                    neighbors.append(next_state)
-            return neighbors
-
-        start = '0000'
         
-        if start in deadends:
+        visited = set(deadends)
+                
+        if  "0000" in visited:
             return -1
+
+        def getNextLevelCombinations(password):
+            output = []
+            for i in range(4):
+                num = int(password[i])
+                for j in [-1,1]:
+                    next_val = (num+j)%10
+                    next_password = password[0:i] + str(next_val) + password[i+1:]
+                    output.append(next_password)
+            return output
         
-        queue = deque([(start, 0)]) # state, steps
-        seen = set(deadends)
-        seen.add(start)
-
+        queue = collections.deque([("0000", 0)])
+        
         while queue:
-            state, steps = queue.popleft()
-            if state == target:
-                return steps 
-
-            for next_state in get_neighbors(state):                
-                if next_state not in seen:
-                    seen.add((next_state))
-                    queue.append((next_state, steps + 1))
-                    
+            password, steps = queue.popleft()
+            
+            if password == target:
+                return steps
+            
+            for next_level in getNextLevelCombinations(password):
+                if next_level not in visited:
+                    visited.add(next_level)
+                    queue.append([next_level, steps+1])
+        
         return -1
