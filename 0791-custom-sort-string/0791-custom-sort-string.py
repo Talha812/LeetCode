d8bdc10213dca1a5feef16a89c9f582a605d206d
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-08-29 22:33:01 +0500

    Time: 49 ms (19.58%), Space: 16.2 MB (94.19%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,26 +1,25 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        
+        
         dic = {}
-        for i in range(len(order)):
-            dic[order[i]] = i
         
-        alph = "abcdefghijklmnopqrstuvwxyz"
-        for ch in alph:
-            if ch not in dic:
-                dic[ch] = 27
-        
-        splt_s = []
         for ch in s:
-            splt_s.append(ch)
+            if ch in dic:
+                dic[ch] += 1
+            else:
+                dic[ch] = 1
         
-        for step in range(1, len(s)):
-            key = splt_s[step]
-            j = step - 1
-                            
-            while j >= 0 and dic[key] < dic[splt_s[j]]:
-                splt_s[j + 1] = splt_s[j]
-                j = j - 1
-
-            splt_s[j + 1] = key
         
-        return "".join(splt_s)
+        ans = ""
+        
+        for ch in order:
+            if ch in dic:
+                ans += (ch*dic[ch])
+                del dic[ch]
+        
+        if dic:
+            for ch, v in dic.items():
+                ans += ch*v
+        
+        return ans
