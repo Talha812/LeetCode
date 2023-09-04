9a0c6e95b249b46a70824c96c0a2263e00a13cd5
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-09-04 18:48:46 +0500

    Time: 260 ms (5.14%), Space: 16.2 MB (70.02%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.py
+++ 2716-minimize-string-length/2716-minimize-string-length.py
@@ -15,12 +15,14 @@ class Solution:
             for j in range(i-1, -1, -1):
                 if arr[j] == arr[i]:
                     arr[j] = -1
+                    break
                     
             # Delete Right
             for j in range(i+1, len(arr)):
                 if arr[j] == arr[i]:
                     arr[j] = -1
-        
+                    break
+                    
         length = 0
         for val in arr:
             if val != -1:
