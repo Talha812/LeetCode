6f19027633b5000a2e7feb225bf5f9b5d0686d77
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-09-04 18:37:21 +0500

    Time: 247 ms (5.14%), Space: 16.3 MB (70.02%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimizedStringLength(self, s: str) -> int:
+        
+        arr = []
+        for ch in s:
+            arr.append(ch)
+        
+        
+        for i in range(len(arr)):
+            
+            if arr[i] == -1:
+                continue
+            
+            # Delete Left
+            for j in range(i-1, -1, -1):
+                if arr[j] == arr[i]:
+                    arr[j] = -1
+                    
+            # Delete Right
+            for j in range(i+1, len(arr)):
+                if arr[j] == arr[i]:
+                    arr[j] = -1
+        
+        length = 0
+        for val in arr:
+            if val != -1:
+                length += 1
+        
+        return length
