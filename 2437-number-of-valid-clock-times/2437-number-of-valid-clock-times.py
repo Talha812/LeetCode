753762fcbd2421af40603e655bb659131259e8c8
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-10-14 19:47:20 +0500

    Time: 32 ms (90.35%), Space: 16.2 MB (77.17%) - LeetHub

--- 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.py
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.py
@@ -6,42 +6,39 @@ class Solution:
         hours = splitted[0]
         minutes = splitted[1]
         
-        ans = 0
+        ans = 1
         if hours == "??":
-            ans += (1*24)
+            ans *= (24)
         
         elif hours[1] == "?":
             if hours[0] == "1" or hours[0] == "0":
-                ans += (1*10)
+                ans *= (10)
             elif hours[0] == "2":
-                ans += (1*4)
+                ans *= (4)
         
         elif hours[0] == "?":
             if int(hours[1]) > 3:
-                ans += (1*2)
+                ans *= (2)
             else:
-                ans += (1*3)
+                ans *= (3)
         
         if minutes == "??":
             if ans == 0:
-                ans += (1*60)
+                ans *= (60)
             else:
-                ans *= (1*60)
+                ans *= (60)
         
         elif minutes[1] == "?":
             if ans == 0:
-                ans += (1*10)
+                ans *= (10)
             else:
-                ans *= (1*10)
+                ans *= (10)
         
         elif minutes[0] == "?":
             if ans == 0:
-                ans += (1*6)
+                ans *= (6)
             else:
-                ans *= (1*6)
-        
-        if ans == 0:
-            ans = 1
-            
+                ans *= (6)
+                    
         return ans
                 
