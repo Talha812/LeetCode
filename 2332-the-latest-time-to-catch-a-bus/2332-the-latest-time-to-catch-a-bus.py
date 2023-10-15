f43e48576a4b6d826315fae10407f902f77be8aa
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-10-15 21:00:00 +0500

    Time: 612 ms (69.57%), Space: 37.2 MB (10.43%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.py
@@ -0,0 +1,27 @@
+class Solution:
+    def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], capacity: int) -> int:
+        buses.sort()
+        passengers.sort()
+        
+        passenger = 0
+        for bus in buses:
+            bussFilled = False
+            curr_cap = 0
+            
+            while passenger < len(passengers) and passengers[passenger] <= bus and curr_cap != capacity:
+                passenger += 1
+                curr_cap += 1
+                
+            if curr_cap == capacity:
+                bussFilled = True
+                
+        if bussFilled:
+            max_seat = passengers[passenger - 1]
+        else:
+            max_seat = buses[-1]
+    
+        booked = set(passengers)
+        for seat in range(max_seat, 0, -1):
+            if seat not in booked:
+                return seat
+            
