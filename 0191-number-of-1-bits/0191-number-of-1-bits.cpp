11ea3883b2ad69eef76ce1050cd3e03238c434ee
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-11-02 18:04:25 +0500

    Time: 4 ms (30.34%), Space: 6.3 MB (44.39%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n!=0){
-            n&= (n-1);
-            count++;
+            count+= n%2;
+            n>>= 1;
         }
         return count;
     }
