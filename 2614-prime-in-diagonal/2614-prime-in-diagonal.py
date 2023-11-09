e588371e51c70d2780b4b08b004b8c3c3587be83
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-11-09 18:48:27 +0500

    Time: 744 ms (29.82%), Space: 28.5 MB (28.39%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.py
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -12,18 +12,33 @@ class Solution:
                 i += 1
             return True
         
-        
+        dic = {}
         prime = 0
         
         for i in range(len(nums)):
             first = nums[i][i]
             sec = nums[i][len(nums)-i-1]
-            
-            if isPrime(first):
+            flag1 = False
+            if first in dic:
+                if dic[first] == True:
+                    prime = max(prime, first)
+                    
+            elif isPrime(first):
+                flag1 = True
                 prime = max(prime, first)
-            
-            if isPrime(sec):
+                
+            dic[first] = flag1
+                
+            flag2 = False
+            if sec in dic:
+                if dic[sec] == True:
+                    prime = max(prime, sec)
+                    
+            elif isPrime(sec):
+                flag2 = True
                 prime = max(prime, sec)
+                
+            dic[sec] = flag2
             
         return prime
     
