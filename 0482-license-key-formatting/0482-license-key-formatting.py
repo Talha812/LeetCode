a903ddd12e8984959e89a779843c60c028a38a8a
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-11-25 18:03:24 +0500

    Time: 78 ms (26.70%), Space: 19.7 MB (12.13%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.py
@@ -0,0 +1,27 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        
+        characters = ""
+        
+        for ch in s:
+            if ch != "-":
+                characters += ch
+        
+        ans = []
+        i = len(characters)-1
+        
+        while i >= 0:
+            buffer = ""
+            c = 0
+            while c < k and i >= 0:
+                buffer = characters[i].upper() + buffer
+                i -= 1
+                c += 1
+            
+            ans.append(buffer)
+        
+        ans.reverse()
+        
+        return "-".join(ans)
+            
+        
