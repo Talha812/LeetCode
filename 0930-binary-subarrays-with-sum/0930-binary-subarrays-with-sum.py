963830d110808aecbe94d80bd77d07b763895e0a
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-12-10 21:20:05 +0500

    Time: 246 ms (77.21%), Space: 20 MB (59.77%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        
+        dic = {0:1}
+        s = 0
+        ans = 0
+        for n in nums:
+            s += n
+            if s-goal in dic:
+                ans += dic[s-goal]
+            
+            if s in dic:
+                dic[s] += 1
+            else:
+                dic[s] = 1
+            
+        return ans
