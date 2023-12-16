457e280b4ad3d6aa5ec6235f625cee4f5a9202d6
Talha812 <72699916+Talha812@users.noreply.github.com>
2023-12-16 21:01:15 +0500

    Time: 82 ms (67.47%), Space: 61.5 MB (58.04%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,19 +1,24 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        
-        # Floyd Warshall Algorithm
-        slow, fast = 0, 0 # 0 is not a part of the cycle
-        while True:
-            slow = nums[slow]       # equivalent to slow.next in linked list
-            fast = nums[nums[fast]] # equivalent to fast.next.next in linked list
-            if slow == fast:       
-                break
-        
-        slow2 = 0
-        # Currently, slow is at the intersection of the slow and fast pointers, which is 
-        # always going to be the same distance from the start of the cycle as slow 2
-        while True:
-            slow = nums[slow]       # equivalent to slow.next in linked list
-            slow2 = nums[slow2]     # equivalent to slow2.next in linked list
-            if slow == slow2:       # will always intersect at the start of the cycle
-                return slow
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(fast == slow)
+                break;
+        }
+
+        int slow2 = 0;
+
+        while(true){
+            slow = nums[slow];
+            slow2 = nums[slow2];
+
+            if(slow == slow2)   return slow;
+        }
+        return -1;
+    }
+};
