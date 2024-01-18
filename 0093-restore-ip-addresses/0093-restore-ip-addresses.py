14b3cb744f01a19975c005a38758eca253cdffb3
Talha812 <72699916+Talha812@users.noreply.github.com>
2024-01-18 19:13:34 +0500

    Time: 65 ms (17.03%), Space: 17.4 MB (30.69%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -3,20 +3,21 @@ class Solution:
         return length == 1 or (s[start] != '0' and (length < 3 or s[start:start+length] <= "255"))
 
     def restoreIpAddresses(self, s):
-        def backtrack(start, path):
-            if start == len(s) and len(path) == 4:
+        stack = [(0, [], 0)]  # (start, path, segment_count)
+        ans = []
+
+        while stack:
+            start, path, segment_count = stack.pop()
+
+            if start == len(s) and segment_count == 4:
                 ans.append(".".join(path))
-                return
-            
+                continue
+
             for length in range(1, 4):
                 if start + length > len(s):
                     break
-                
+
                 if self.valid(s, start, length):
-                    backtrack(start + length, path + [s[start:start+length]])
-        
-        ans = []
-        backtrack(0, [])
+                    stack.append((start + length, path + [s[start:start+length]], segment_count + 1))
+
         return ans
-
-
