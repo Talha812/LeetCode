6f9388e839af14b11cab77cd9d954e8f4c0d97b9
Talha812 <72699916+Talha812@users.noreply.github.com>
2024-08-18 11:25:43 +0500

    Time: 4055 ms (56.17%), Space: 16.2 MB (98.95%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,30 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        rows = len(board)
-        columns = len(board[0])
-
-        def find(board, word, i, j):
-            if len(word)==0: 
+        def dfs(board, word, i, j):
+            if len(word) == 0: 
                 return True
             
-            if i<0 or i >= rows or j<0 or j >= columns or board[i][j] != word[0] or board[i][j]==' ': 
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[0] or board[i][j]==-1: 
                 return False
             
-            c = board[i][j]
-            board[i][j] = ' '
+            rec_char = board[i][j]
+            board[i][j] = -1
             word = word[1:]
-            up = find(board, word, i-1, j)
-            down = find(board, word, i+1, j)
-            left = find(board, word, i, j-1)
-            right = find(board, word, i, j+1)
-            res = up or down or left or right
-            board[i][j] =c
-            return res
+            
+            left = dfs(board, word, i, j-1)
+            right = dfs(board, word, i, j+1)
+            up = dfs(board, word, i-1, j)
+            down = dfs(board, word, i+1, j)
+            
+            board[i][j] = rec_char
+            return up or down or left or right
         
-        for r in range(rows):
-            for c in range(columns):
-                if (board[r][c] == word[0]) and find(board, word, r,c):
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if (board[r][c] == word[0]) and dfs(board, word, r,c):
                     return True
                 
         return False
